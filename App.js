cf8a6920afbc4f1c55da3b9c0b39ed226cfca709
IlyasDiker <ilyasbenhssine@gmail.com>
2021-05-13 22:28:20 +0000

    test buttons + buttons

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Alert, Image, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, Button } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Rolls Royce TEST</Text>
-      <TouchableOpacity onPress={() => console.log('test')}>
+      <TouchableNativeFeedback onPress={() => console.log('test')}>
+        
+        <View style={{ width: 200, height:200, backgroundColor: "dodgerblue" }}>
         <Image 
         source={{ 
-          width: 200,
-          height: 200,
+          width: 100,
+          height: 100,
           uri: "https://i.imgur.com/oPgSc6Q.jpg"}} />
-          </TouchableOpacity>
+        </View>
+      </TouchableNativeFeedback>
+      <Button 
+        color="orange"
+        title="Click me" 
+        onPress={() => Alert.alert("Title", "Message", [
+          {text: "Yes", onPress: ()=>console.log("Yes")},
+          {text: "No", onPress: ()=>console.log("No")},
+        ])} />
+        {/* <Button 
+        color="green"
+        title="Prompt Alert" 
+        onPress={() => Alert.prompt("Are you okay?", "Hmm", text => console.log(text))} /> */}
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
